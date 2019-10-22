126640c9054e78fa71b54de1e0338c8562a7640a
aaron.hines <aaron.hines@OUTPOST.LOCAL>
2019-10-22 12:58:30 -0400

    Working on game states

--- duel_masters/Assets/Architecture/Framework/GameState/GameStateManager.cs
+++ duel_masters/Assets/Architecture/Framework/GameState/GameStateManager.cs
@@ -7,20 +7,49 @@ using Sirenix.OdinInspector;
 
 namespace GameFramework.GameState
 {
-    class GameStateManager : Singleton_SerializedMonobehaviour<GameStateManager>
+    // TODO: make states take a priority
+    public enum StatePriority : int
     {
-        public GameStateIdentifier currentState
+        Low,
+        High    
+    }
+
+    public class State
+    {
+        public State(string owner, GameStateIdentifier gameState, StatePriority priority)
         {
-            get;
-            private set;
+            this.owner = owner;
+            this.gameState = gameState;
+            this.priority = priority;
         }
 
-        public List<GameStateIdentifier> gameStates
+        public string owner = "unidentified";
+        public GameStateIdentifier gameState;
+        public StatePriority priority;
+    }
+
+    class GameStateManager : Singleton_SerializedMonobehaviour<GameStateManager>
+    {
+
+        public State currentState
+        {
+            get
+            {
+                if(gameStates.Count > 0)
+                {
+                    return gameStates.Peek();
+                }
+
+                return null;
+            }
+        }
+
+        public Stack<State> gameStates
         {
             get;
             private set;
 
-        } = new List<GameStateIdentifier>();
+        } = new Stack<State>();
 
         public GameStateChangedEvent gameStateChangedEvent
         {
@@ -28,31 +57,43 @@ namespace GameFramework.GameState
             private set;
         } = new GameStateChangedEvent();
 
-        /// <summary>
-        ///     Start this state
-        /// </summary>
-        /// <param name="gameState"></param>
-        public void ForceState(GameStateIdentifier gameState)
+        public void PushState(string owner, GameStateIdentifier gameState, StatePriority statePriority = StatePriority.Low)
         {
+            State _state = new State(owner, gameState, statePriority);
+            if (currentState != null)
+            {
+                if(statePriority < currentState.priority)
+                {
+                    // insert below all of the other ones
+                    Stack<State> _states = new Stack<State>(gameStates);
+                    while(_states.Peek().priority > statePriority && _states.Count > 0)
+                    {
+                        _states.Pop();
+                    }
 
-        }
+                    _states.Push( _state );
+                    return;
+                }
+            }
 
-        /// <summary>
-        ///     Start this state
-        /// </summary>
-        /// <param name="gameState"></param>
-        public void PushState(GameStateIdentifier gameState)
-        {
-
+            gameStates.Push( _state );
         }
 
         /// <summary>
         /// End this state and return to default
         /// </summary>
         /// <param name="gameState"></param>
-        public void PopState(GameStateIdentifier gameState)
+        public bool PopState(GameStateIdentifier gameState)
         {
-
+            if(currentState.gameState == gameState)
+            {
+                return true;
+            }
+            else
+            {
+                Debug.LogWarning("Tried to pop a state that is not the current state");
+                return false;
+            }
         }
     }
 }
